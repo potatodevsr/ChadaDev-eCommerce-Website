f202967975d9f244eb07c9c46dea772644061105
potatodevsr <hoshiku@gmail.com>
2023-08-12 17:44:29 +0700

    upd section blog

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,21 @@
 'use strict';
 
-/**
- * navbar toggle
- */
-
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 
 const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+// Display current date
+const currentDate = new Date();
+const currentDateElement = document.getElementById('currentDate');
+
+const day = String(currentDate.getDate()).padStart(2, '0');
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const year = currentDate.getFullYear();
+
+currentDateElement.textContent = `${day}/${month}/${year}`;
 
 for (let i = 0; i < navElemArr.length; i++) {
     navElemArr[i].addEventListener("click", function () {
@@ -19,14 +25,10 @@ for (let i = 0; i < navElemArr.length; i++) {
 }
 
 
-
-/**
- * add active class on header when scrolled 200px from top
- */
-
 const header = document.querySelector("[data-header]");
 
 window.addEventListener("scroll", function () {
     window.scrollY >= 200 ? header.classList.add("active")
         : header.classList.remove("active");
 })
+
